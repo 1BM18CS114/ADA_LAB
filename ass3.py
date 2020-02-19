f3cd57334fa28bc19a518e39bd9f5d47741bc758
1BM18CS114 <54112164+1BM18CS114@users.noreply.github.com>
2020-02-19 12:59:26 +0530

    Update ass3.py

--- ass3.py
+++ ass3.py
@@ -1,5 +1,13 @@
+import time
+from random import randint
 #Input Array
-arr = [12, 7, 1, 5, 4, 6]
+leng = 1000
+print(leng)
+arr = []
+for i in range(leng):
+	val = randint(1, 10000)
+	arr.append(val)
+#arr = [12, 7, 1, 5, 4, 6]
 
 #Selection Sort 
 def sel_sort(arr):
@@ -12,18 +20,38 @@ def sel_sort(arr):
 		a = arr[i]
 		arr[i] = arr[temp]
 		arr[temp] = a
-		print(arr)
+		#print(arr)
 	return arr
 	
-
-print(sel_sort(arr))
+start = time.time()
+sel_sort(arr)
+end = time.time()
+print(end - start)
 
 #K largest Values
 def k_larg(array, k):
 	arr = sel_sort(array)
 	return arr[-k:]
 
-print(k_larg(arr, 3))
+def k_larg2(arr, k):
+	for i in range(len(arr) - k):
+		temp = i
+		for j in range(i, len(arr)):
+			if arr[temp] > arr[j]:
+				temp = j
+				#print(temp)
+		a = arr[i]
+		arr[i] = arr[temp]
+		arr[temp] = a
+		#print(arr)
+		
+	return arr[-k:]
+
+start = time.time()
+k_larg2(arr, 3)
+end = time.time()
+print(end - start)
 	
+		
 		
 		
